f8db4fa813a83e81d50f7e6ed286b51e1d59eb7a
Gabriel05P <gabriel.pervorfi@edu.tbz.ch>
2023-07-04 08:28:05 +0200

    update

--- prepareFiles.sh
+++ prepareFiles.sh
@@ -1,41 +1,53 @@
+
 #!/bin/bash
 
-# Erstelle das Template-Verzeichnis und die Dateien
-
-# Verzeichnis anlegen
-mkdir _template 2>/dev/null
-
-touch _template/file{1..3}.txt
-
-namensdatei="M122-PE22a.txt"
-
-verzeichnis_namensdateien="_namensdateien"
-mkdir "$verzeichnis_namensdateien" 2>/dev/null
-
-# Namendatei anlegen und Inhalte reinschreiben
-echo -e "Amherd\nBaume-Schneider\nBerset\nCassis\nKeller-Sutter\nParmelin\nRoesti" > "$verzeichnis_namensdateien/$namensdatei"
-
-# Gleich noch eine zweite Namensdatei
-namensdatei="M122-PE22b.txt"
-echo -e "Burkhart\nChiesa\nJositsch\nNoser\nArlind\nHarun" > "$verzeichnis_namensdateien/$namensdatei"
-
-# erstellt alle Verzeichnisse und kopiert die
-# Template-Dateien in alle Teilnehmer aller Klassen
 
 verzeichnis_template="_template"
+mkdir "$verzeichnis_template" 2>/dev/null
+cd "$verzeichnis_template"
+
+# Vorlagendateien erstellen
+echo "datei-1.txt" > datei-1.txt
+echo "datei-2.docx" > datei-2.docx
+echo "datei-3.pdf" > datei-3.pdf
+echo "datei-4.xlsx" > datei-4.xlsx
+
+cd ..
+
+# Verzeichnis für die Namensdateien erstellen
 verzeichnis_namensdateien="_namensdateien"
+mkdir "$verzeichnis_namensdateien" 2>/dev/null
+cd "$verzeichnis_namensdateien"
 
-allFiles=($(ls -1 "$verzeichnis_namensdateien"))
+# Erste Namensdatei erstellen
+namensdatei="M122-BR23a.txt"
+echo "Amherd" > "$namensdatei"
+echo "Baume-Schneider" >> "$namensdatei"
+echo "Berset" >> "$namensdatei"
+echo "Cassis" >> "$namensdatei"
+echo "Keller-Sutter" >> "$namensdatei"
+echo "Parmelin" >> "$namensdatei"
+echo "Roesti" >> "$namensdatei"
 
-for file in "${allFiles[@]}"; do
-  fileNameVorPunkt="${file%.*}"
-  klasse="$fileNameVorPunkt"
-  fileinhalt=$(cat "$verzeichnis_namensdateien/$file")
+# Zweite Namensdatei erstellen
+namensdatei="M122-SR23a.txt"
+echo "Burkhart" > "$namensdatei"
+echo "Chiesa" >> "$namensdatei"
+echo "Jositsch" >> "$namensdatei"
+echo "Noser" >> "$namensdatei"
 
-  IFS=$'\n' read -d '' -r -a teilnehmerArray <<< "$fileinhalt"
+cd ..
 
-  for teilnehmer in "${teilnehmerArray[@]}"; do
-    mkdir -p "$verzeichnis_namensdateien/$klasse/$teilnehmer"
-    cp "$verzeichnis_template"/* "$verzeichnis_namensdateien/$klasse/$teilnehmer"
-  done
+# Iteration über Namensdateien
+allFiles=$(ls "$verzeichnis_namensdateien")
+
+for file in $allFiles; do
+        fileNameVorPunkt="${file%.*}"
+        klasse="$fileNameVorPunkt"
+        fileinhalt=$(cat "$verzeichnis_namensdateien/$file")
+        IFS=$'\n' read -rd '' -a teilnehmer_arr <<< "$fileinhalt"
+        for teilnehmer in "${teilnehmer_arr[@]}"; do
+                mkdir -p "$verzeichnis_namensdateien/$klasse/$teilnehmer"
+                cp "$verzeichnis_template"/* "$verzeichnis_namensdateien/$klasse/$teilnehmer/"
+        done
 done
