e11f6bdae2712e1ccc7dd365be59553c40e4d53c
Gabriel05P <118826885+Gabriel05P@users.noreply.github.com>
2023-07-04 10:55:59 +0200

    Update prepareFiles.sh

--- prepareFiles.sh
+++ prepareFiles.sh
@@ -4,46 +4,36 @@
 
 verzeichnis_template="_template"
 mkdir "$verzeichnis_template" 2>/dev/null
-cd "$verzeichnis_template"
+
 
 # Vorlagendateien erstellen
-echo "datei-1.txt" > datei-1.txt
-echo "datei-2.docx" > datei-2.docx
-echo "datei-3.pdf" > datei-3.pdf
-echo "datei-4.xlsx" > datei-4.xlsx
+echo "datei-1.txt" > $verzeichnis_template/datei-1.txt
+echo "datei-2.docx" > $verzeichnis_template/datei-2.docx
+echo "datei-3.pdf" > $verzeichnis_template/datei-3.pdf
+echo "datei-4.xlsx" > $verzeichnis_template/datei-4.xlsx
 
-cd ..
 
 # Verzeichnis für die Namensdateien erstellen
 verzeichnis_namensdateien="_namensdateien"
 mkdir "$verzeichnis_namensdateien" 2>/dev/null
-cd "$verzeichnis_namensdateien"
+
 
 # Erste Namensdatei erstellen
 namensdatei="M122-BR23a.txt"
-echo "Amherd" > "$namensdatei"
-echo "Baume-Schneider" >> "$namensdatei"
-echo "Berset" >> "$namensdatei"
-echo "Cassis" >> "$namensdatei"
-echo "Keller-Sutter" >> "$namensdatei"
-echo "Parmelin" >> "$namensdatei"
-echo "Roesti" >> "$namensdatei"
+echo -e "Amherd\nBaume-Schneider\nBerset\nCassis\nKeller-Sutter\nParmelin\nRoesti" > "$verzeichnis_namensdateien/$namensdatei"
+
 
 # Zweite Namensdatei erstellen
 namensdatei="M122-SR23a.txt"
-echo "Burkhart" > "$namensdatei"
-echo "Chiesa" >> "$namensdatei"
-echo "Jositsch" >> "$namensdatei"
-echo "Noser" >> "$namensdatei"
+echo -e "Burkhart\nChiesa\nJositsch\nNoser" > "$verzeichnis_namensdateien/$namensdatei"
 
-cd ..
+
 
 # Iteration über Namensdateien
 allFiles=$(ls "$verzeichnis_namensdateien")
 
 for file in $allFiles; do
-        fileNameVorPunkt="${file%.*}"
-        klasse="$fileNameVorPunkt"
+        klasse="${file%.*}"
         fileinhalt=$(cat "$verzeichnis_namensdateien/$file")
         IFS=$'\n' read -rd '' -a teilnehmer_arr <<< "$fileinhalt"
         for teilnehmer in "${teilnehmer_arr[@]}"; do
